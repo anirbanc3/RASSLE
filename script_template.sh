0d53eab899e3df41a4ea121a6d3caed36b7f8a7e
Anirban Chakraborty <ch.anirban00727@gmail.com>
2021-01-24 02:03:31 +0530

    Add files via upload

---
+++ script_template.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gcc -o ras2 trial_2.c -w
+gcc -o ecc ecc_encrypt.c -w -lcrypto
+
+dec=128
+
+for i in {1..32};	do
+	var2=$dec
+	hex=$(echo "obase=16; $dec"| bc);
+	for j in {1..10000};	do
+		(chrt -d --sched-runtime 3600 --sched-deadline 3700 --sched-period 7200 0 ./ras2 $dec $j &
+		chrt -d --sched-runtime 3600 --sched-deadline 3700 --sched-period 7200 0 ./ecc $hex) 
+
+	done
+	dec=`expr $dec + 4`
+done
+
+rm ras2 ecc
+
+echo "done"
